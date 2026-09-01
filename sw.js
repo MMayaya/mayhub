cb4cc148db92d117ad06a1faabb819318d0165ac
MMayaya <mzwamadodamayaya@gmail.com>
2026-09-01 21:52:30 +0200

    Game tags

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CORE_CACHE_NAME = 'may-learning-core-v57-grade10-activities';
+const CORE_CACHE_NAME = 'may-learning-core-v58-quest-analytics';
 const RUNTIME_CACHE_NAME = 'may-learning-runtime-pages';
 const CERTIFICATE_DOWNLOAD_CACHE = 'may-learning-certificate-downloads';
 const CERTIFICATE_DOWNLOAD_PATH = '/certificate-download/';
-const SERVICE_WORKER_VERSION = '73-grade10-term3-activities';
+const SERVICE_WORKER_VERSION = '74-quest-analytics';
 
 const CORE_ASSETS = [
   '/universal-search.css',
